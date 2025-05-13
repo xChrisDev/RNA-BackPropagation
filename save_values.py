f87f5771f3ead2a73bf920a253bc77ce5f0f7ce5
xChrisDev <chrissarmiento200203@gmail.com>
2025-05-12 21:58:27 -0600

    feat: predictions

--- save_values.py
+++ save_values.py
@@ -1,11 +1,22 @@
+import pandas as pd
 from colorama import Fore
 from messages import message_color
-import pandas as pd
-
 
 def save(wij, wjk, theta_j, theta_k):
-    pd.DataFrame(wij).to_csv("data/weights_ij.csv", index=False, header=False)
-    pd.DataFrame(wjk).to_csv("data/weights_jk.csv", index=False, header=False)
-    pd.DataFrame(theta_j).to_csv("data/theta_j.csv", index=False, header=False)
-    pd.DataFrame(theta_k).to_csv("data/theta_k.csv", index=False, header=False)
+    pd.DataFrame(wij).to_csv("data/weights_ij.csv", index=False, header=False, mode='w')
+    pd.DataFrame(wjk).to_csv("data/weights_jk.csv", index=False, header=False, mode='w')
+    pd.DataFrame(theta_j).to_csv("data/theta_j.csv", index=False, header=False, mode='w')
+    pd.DataFrame(theta_k).to_csv("data/theta_k.csv", index=False, header=False, mode='w')
     message_color("PESOS GUARDADOS", Fore.MAGENTA)
+
+def load():
+    try:
+        wij = pd.read_csv("data/weights_ij.csv", header=None).values
+        wjk = pd.read_csv("data/weights_jk.csv", header=None).values
+        theta_j = pd.read_csv("data/theta_j.csv", header=None).values.flatten()
+        theta_k = pd.read_csv("data/theta_k.csv", header=None).values.flatten()
+        message_color("PESOS CARGADOS", Fore.MAGENTA)
+        return wij, wjk, theta_j, theta_k
+    except FileNotFoundError:
+        message_color("Error: No se encontraron archivos de pesos guardados.", Fore.RED)
+        raise
